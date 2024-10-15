c363a6f5ecde8bf79b57f1c0c7f0aeca8f0988c2
Nischal-YT <nischalnayak05@gmail.com>
2024-10-15 16:14:59 +0530

    Initial commit

--- src/test/java/com/thetesting/ex_selenium/ex_001.java
+++ src/test/java/com/thetesting/ex_selenium/ex_001.java
@@ -1,2 +1,12 @@
-package com.thetesting.ex_selenium;public class ex_001 {
+package com.thetesting.ex_selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ex_001 {
+
+    public static void main(String[] args) {
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+    }
 }
